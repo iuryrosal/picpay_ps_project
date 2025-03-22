f7f5e2dc82d7d3d387ef5e4d0aef556278925c31
iuryrosal <iuryrosal@gmail.com>
2025-03-21 22:26:19 -0300

    feat: add sqlite_client

---
+++ db/sqllite_client.py
@@ -0,0 +1,21 @@
+from typing import Any
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+SqLiteBase = declarative_base()
+
+class SqLiteClient:
+    database_path = 'sqlite:db/database.db'
+
+    def __init__(self) -> None:
+        self._engine = create_engine(self.database_path)
+        self._session = sessionmaker(bind=self.engine, autocommit=False, autoflush=False)
+    
+    def __call__(self) -> Session:
+        session_local = self._session()
+        try:
+            yield session_local
+        finally:
+            session_local.close()
