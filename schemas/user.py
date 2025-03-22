cd29de1d46c2f1589fc3db9254382325c1eb15f1
iuryrosal <iuryrosal@gmail.com>
2025-03-21 22:28:22 -0300

    feat(UserSchema): add email validation in schema

--- schemas/user.py
+++ schemas/user.py
@@ -1,13 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 from typing import Optional
 from datetime import datetime
 
 
-class User(BaseModel):
+class UserSchema(BaseModel):
     id: str
     first_name: str
     last_name: Optional[str]
-    email: str
+    email: EmailStr
     created_at: datetime
     updated_at: Optional[datetime]
