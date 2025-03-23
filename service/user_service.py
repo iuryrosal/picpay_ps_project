7ad664c5230831ad270ccc274627a94d0c47ea16
iuryrosal <iuryrosal@gmail.com>
2025-03-23 00:54:46 -0300

    ♻️ refactor(user_service): add general handle response

--- service/user_service.py
+++ service/user_service.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List, Union, Tuple
 from repositories.meta.interface_user_repository import IUserRepository
 from models.user_model import UserModel
 from service.meta.interface_user_service import IUserService
@@ -8,37 +8,30 @@ class UserService(IUserService):
     def __init__(self, repository: IUserRepository):
         self.repository = repository
 
-    def create_user(self, first_name: str, last_name: str, email: str) -> Union[UserModel, str]:
+    def _handle_response_from_repository(user: Union[UserModel, List[UserModel], None],
+                                         error_type: Union[str, None],
+                                         error_msg: Union[str, None]) -> Union[UserModel, List[UserModel], Tuple[str, str]]:
+        if user:
+            return user
+        else:
+            return (error_type, error_msg)
+
+    def create_user(self, first_name: str, last_name: str, email: str) -> Union[UserModel, Tuple[str, str]]:
         user, error_type, error_msg = self.repository.create(first_name, last_name, email)
-        if user:
-            return user
-        else:
-            return f"{error_type}: {error_msg}"
+        self._handle_response_from_repository(user, error_type, error_msg)
 
-    def get_user(self, user_id: int) -> Union[UserModel, str]:
+    def get_user(self, user_id: int) -> Union[UserModel, Tuple[str, str]]:
         user, error_type, error_msg = self.repository.select_by_id(user_id)
-        if user:
-            return user
-        else:
-            return f"{error_type}: {error_msg}"
+        self._handle_response_from_repository(user, error_type, error_msg)
 
-    def get_all_users(self) -> Union[List[UserModel], str]:
+    def get_all_users(self) -> Union[List[UserModel], Tuple[str, str]]:
         users, error_type, error_msg  = self.repository.select_all()
-        if users:
-            return users
-        else:
-            return f"{error_type}: {error_msg}"
+        self._handle_response_from_repository(users, error_type, error_msg)
 
-    def update_user(self, user_id: int, first_name: str, last_name: str, email: str) -> Union[UserModel, str]:
+    def update_user(self, user_id: int, first_name: str, last_name: str, email: str) -> Union[UserModel, Tuple[str, str]]:
         user, error_type, error_msg  = self.repository.update(user_id, first_name, last_name, email)
-        if user:
-            return user
-        else:
-            return f"{error_type}: {error_msg}"
+        self._handle_response_from_repository(user, error_type, error_msg)
 
-    def delete_user(self, user_id: int) -> Union[UserModel, str]:
+    def delete_user(self, user_id: int) -> Union[UserModel, Tuple[str, str]]:
         user, error_type, error_msg  = self.repository.delete_by_id(user_id)
-        if user:
-            return user
-        else:
-            return f"{error_type}: {error_msg}"
+        self._handle_response_from_repository(user, error_type, error_msg)
