0ab7040f1466e3aba9359a69313c5a1a4d21ca92
iuryrosal <iuryrosal@gmail.com>
2025-03-23 19:08:19 -0300

    📝 docs(sqlite_client): add docstrings

--- db/sqllite_client.py
+++ db/sqllite_client.py
@@ -7,6 +7,14 @@ from sqlalchemy.orm.session import Session
 SqLiteBase = declarative_base()
 
 class SqLiteClient:
+    """Cliente para realizar conexão com o banco de dados SqLite responsável por dados de usuários (User) atrelado ao caminho db/database.db,
+
+    O uso do objeto instanciado dessa classe como uma execução de função (__call__) é retornado um gerador que é responsável por ativar a sessão e encerrando a sessão no final da iteração. Pode ser utilizado como gerenciador de contexto.
+
+    Atributos de Instância:
+        _engine: Responsável por instanciar um objeto engine atrelado ao banco de dados e drivers necessários.
+        _session: Responsável por instanciar um objeto Session utilizando o _engine para realizar as operações necessárias.
+    """
     database_path = "sqlite:///db/database.db"
 
     def __init__(self) -> None:
