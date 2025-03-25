46b0aca22ac8e82c252cf204c93c5e5885e03ed6
iuryrosal <iuryrosal@gmail.com>
2025-03-25 10:58:37 -0300

    ♻️ refactor(user_controller): Adjust verification of error_tuple

--- controller/v1/user_controller.py
+++ controller/v1/user_controller.py
@@ -22,6 +22,21 @@ class UserController:
     """
     router = APIRouter()
 
+    def __verify_error_tuple(error_tuple:  Tuple[str, str]):
+        def is_tuple(obj):
+            return isinstance(obj, tuple)
+        
+        def is_tuple_with_2_items(obj):
+            return len(obj) == 2
+        
+        def is_tuple_only_str(obj):
+            return all(isinstance(item, str) for item in error_tuple)
+
+        if not is_tuple(error_tuple):
+            raise Exception(f"Service error tuple response have invalid format: {type(error_tuple)}")
+        elif not is_tuple_with_2_items(error_tuple) and is_tuple_only_str(error_tuple):
+            raise Exception(f"Service error tuple response have invalid values: {error_tuple}")
+
     def __handle_error_response_from_service(error_tuple:  Tuple[str, str]):
         """Em caso de falha detectada durante processamento da requisição, irá processar a tupla de erros (Título (str), Descrição (str)) gerando uma exceção em caso de tupla inválida e, em caso válido, gerará uma response padronizada de erro (JsonResponse).
 
@@ -35,11 +50,8 @@ class UserController:
         Returns:
             JSONResponse: Padrão de resposta de requisição com status_code do HTTP e conteúdo no padrão do schema GenericErrorResponse, contendo status por texto, código do erro (título do erro), mensagem com a descrição do erro e detalhes.
         """
-        if not isinstance(error_tuple, tuple) and len(error_tuple) == 2 and all(isinstance(item, str) for item in error_tuple):
-            raise Exception(f"Service error tuple response have invalid format: {type(error_tuple)}")
-        elif not len(error_tuple) == 2 and not all(isinstance(item, str) for item in error_tuple):
-            raise Exception(f"Service error tuple response have invalid values: {error_tuple}")
-
+        UserController.__verify_error_tuple(error_tuple)
+        
         if error_tuple[0] == "UserNotExists":
             http_status_obj = http.HTTPStatus.NOT_FOUND
             response = GenericErrorResponse(
